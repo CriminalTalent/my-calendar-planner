d8bfa8ca44bbda51a3919d7d9d2e2582d1d46623
CriminalTalent <rntjdrud@naver.com>
2025-09-24 22:36:01 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-const repoName = 'my-calendar-planner'; // 저장소 이름
+const repoName = 'my-calendar-planner'; // 본인 저장소명
 
 export default defineConfig({
   plugins: [react()],
-  base: `/${repoName}/`, // GitHub Pages(프로젝트 페이지)용 경로
+  base: `/${repoName}/`,
 });
